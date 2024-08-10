69d6e7ac041a6e5e22033350a2138bb85af9ab2f
leo0263 <leo.0263@gmail.com>
2024-08-10 13:41:41 +0700

    add save state, to provide move history

--- src/App.js
+++ src/App.js
@@ -74,13 +74,32 @@ function Board({ xTurn, squares, onPlay }) {
 
 function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const [xTurn, setXTurn] = useState(true);
-  const currentSquares = history[history.length -1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const xTurn = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
-    setXTurn(!xTurn);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) description = 'Go to move #' + move;
+    else description = 'Go to game start'
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}> {description} </button>
+      </li>
+    )
+    
+  });
 
   return (
     <div className="game">
@@ -88,7 +107,7 @@ function Game() {
         <Board xTurn={xTurn} squares={currentSquares} onPlay={handlePlay}/>
       </div>
       <div className="game-info">
-        <ol>{/* TODO */}</ol>
+        <ol> {moves} </ol>
       </div>
     </div>
   )
